1922e91c2d36fc1e0dc050ed725d9c207495b2e3
thoriqagfi <thoriq.aghfi60@gmail.com>
2023-11-28 19:26:12 +0700

    feat: setup database, routes, and user controller

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,13 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+
 $routes->get('/', 'Home::index');
+$routes->get('/login', 'Home::login');
+$routes->get('/register', 'Home::register');
+
+$routes->get('/user', 'UserController::index');
+$routes->post('/user/save', 'UserController::save');
+$routes->get('/user/login', 'UserController::login');
+$routes->post('/user/auth', 'UserController::auth');
+$routes->get('/user/delete/(:segment)', 'UserController::delete/$1');
--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,14 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return view('welcome_message');
+        return view('index');
+    }
+    public function login(): string
+    {
+        return view('login');
+    }
+    public function register(): string
+    {
+        return view('register');
     }
 }
